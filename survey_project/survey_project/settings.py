c2bc05f87158c81c94007d86dd4a34642ac0fa47
Vinithkumar001 <122115335+Vinithkumar001@users.noreply.github.com>
2023-11-09 16:08:27 +0530

    Update settings.py

--- survey_project/survey_project/settings.py
+++ survey_project/survey_project/settings.py
@@ -77,16 +77,19 @@ WSGI_APPLICATION = 'survey_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'CRUD_survey',
-        'USER':'postgres',
-        'PASSWORD':'Vinithk1234$',
-        'HOST' : 'localhost',
-        'PORT' :  '5432' ,
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 
 # Password validation
