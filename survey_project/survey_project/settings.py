64c883d55116237be2c56bcf5d8903513a2c73a2
Vinithkumar001 <122115335+Vinithkumar001@users.noreply.github.com>
2023-11-14 12:57:52 +0530

    Update settings.py

--- survey_project/survey_project/settings.py
+++ survey_project/survey_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--dg7-bc48jm1&p2c1y6p_%y*j!&qzjk3xz1-k!yy@-z5r4b0u3
 DEBUG = True
 
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","localhoat:5001"]
 
 
 
